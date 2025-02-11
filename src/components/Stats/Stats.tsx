e4332d8c3c251954ab5ef865f94d8f9f5c643641
JASolarteR <j.solarte@globant.com>
2025-02-11 15:07:23 -0500

    [UPDATE] Update stats section, add progress bar and styling

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { useTasks } from "../../hooks/useTasks";
+import "./style.css"
 
 export const Stats = () => {
   const { tasks } = useTasks();
 
-  const completedTasks = tasks.filter(({ completed }:{completed:boolean}) => completed).length;
-  const pendingTasks = tasks.filter(({ completed }:{completed:boolean}) => !completed).length
-  const completionRate = 1 / (tasks.length / completedTasks) * 100 || 0
+  const completedTasks = tasks.filter(
+    ({ completed }: { completed: boolean }) => completed
+  ).length;
+  const pendingTasks = tasks.filter(
+    ({ completed }: { completed: boolean }) => !completed
+  ).length;
+  const completionRate = (1 / (tasks.length / completedTasks)) * 100 || 0;
 
   return (
     <section className="stats-section">
-      <h3>Statistics</h3>
       <div className="stats-list">
         <p>Completed tasks: {completedTasks}</p>
+          <label htmlFor="tasks-progress" className="tasks-progress">Tasks progression
+          <progress
+            max={100}
+            id="tasks-progress"
+            value={completionRate.toPrecision(3)}
+          >
+            {completionRate.toPrecision(3)}%
+          </progress>
+          </label>
         <p>Pending tasks: {pendingTasks}</p>
-        <p>Completion rate: {completionRate.toPrecision(3)}%</p>
       </div>
     </section>
   );
